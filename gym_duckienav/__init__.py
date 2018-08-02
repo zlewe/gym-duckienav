a1f5aa20ce6538c61623f4e23471c7178af2927c
Yan Hung 'Allen' Ou <bicycleboy323@gmail.com>
2018-08-02 22:24:25 +0800

    Update __init__.py

--- gym_duckienav/__init__.py
+++ gym_duckienav/__init__.py
@@ -3,5 +3,7 @@ from gym.envs.registration import register
 register(
     id='DuckieNav-v0',
     entry_point='gym_duckienav.envs:DuckieNavEnv',
+    id='DuckieNav-v1',
+    entry_point='gym_duckienav.envs:DuckieNavEnvV2'
 )
 
