d56189e69b9087134d361c0760fd1450f4e90901
hchengwang <hchengwang@gmail.com>
2018-08-08 11:43:01 +0800

    revise __init__ register

--- gym_duckienav/__init__.py
+++ gym_duckienav/__init__.py
@@ -2,8 +2,11 @@ from gym.envs.registration import register
 
 register(
     id='DuckieNav-v0',
-    entry_point='gym_duckienav.envs:DuckieNavEnv'
-    #id='DuckieNav-v2',
-    #entry_point='gym_duckienav.envs:DuckieNavEnvV2'
+    entry_point='gym_duckienav.envs:DuckieNavEnv',
+)
+
+register(
+    id='DuckieNav-v2',
+    entry_point='gym_duckienav.envs:DuckieNavEnvV2'
 )
 
